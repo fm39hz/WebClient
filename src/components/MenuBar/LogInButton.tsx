e701e968a1cfae0cdd3e8a72e1e23e7831445b8e
fm39hz <hitpoint2k3@gmail.com>
2023-12-13 13:56:00 +0700

    Complete designing LoginButton

--- src/components/MenuBar/LogInButton.tsx
+++ src/components/MenuBar/LogInButton.tsx
@@ -1,11 +1,11 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 import { UserIcon } from '@heroicons/react/24/solid'
 import { ButtonBase } from '@mui/material'
 
 const LogInButton = () => (
-	<ButtonBase className="">
-		<UserIcon className="w-12 h-12 text-white" />
-		<Text>Đăng nhập</Text>
+	<ButtonBase className="border-2 rounded-xl" onClick={() => {}}>
+		<UserIcon className="w-12 h-12" />
+		<Text className="mr-2 text-justify">Đăng nhập</Text>
 	</ButtonBase>
 )
 
