0a68f90b4f8f105936d7339368abe248abca584a
fm39hz <hitpoint2k3@gmail.com>
2023-12-14 12:08:31 +0700

    apply new path

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@chakra-ui/react';
 import MenuBar from '../components/MenuBar/Menubar';
-import MainPages from '../components/MainView/MainPages';
+import MainPages from '../components/PageRouting/MainPages';
 
 const App = () => (
 	<Flex className="flex-col bg-black text-white h-screen w-full">
