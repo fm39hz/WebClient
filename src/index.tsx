82d1a8070417ec1e825a6b07859b0a1af432d35f
fm39hz <hitpoint2k3@gmail.com>
2023-12-14 11:59:13 +0700

    apply semicolon

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import 'tailwindcss/tailwind.css'
-import App from 'pages/App'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+import 'tailwindcss/tailwind.css';
+import App from 'pages/App';
+import { BrowserRouter } from 'react-router-dom';
 
-const container = document.getElementById('root') as HTMLDivElement
-const root = createRoot(container)
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
 
 root.render(
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
-)
+);
