3e1c840c93c444cf62d95b18a37d8a9fc70acaa9
fm39hz <hitpoint2k3@gmail.com>
2023-12-17 22:14:23 +0700

    added IsSignedIn Props

--- src/Constant.tsx
+++ src/Constant.tsx
@@ -1,9 +1,10 @@
 export const enum ServiceEndPoint {
 	products = '/Products',
 	login = '/Auth/Login',
+	isLoggedIn = '/Users/IsLoggedIn',
 	cpu = '/Cpus',
 	users = '/Users',
 }
 export const WebServiceApi = 'http://localhost:5234/api';
-export const GetApi = (endPoint: ServiceEndPoint) =>
+export const GetApi = (endPoint: ServiceEndPoint): string =>
 	WebServiceApi.concat(endPoint);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,27 @@
 import { Flex } from '@chakra-ui/react';
 import MenuBar from '../components/MenuBar/Menubar';
 import MainPages from '../components/PageRouting/MainPages';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { GetApi, ServiceEndPoint } from 'Constant';
 
 const App = () => {
 	const [isSingedIn, setIsSingedIn] = useState(false);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					GetApi(ServiceEndPoint.isLoggedIn).concat(
+						'/' + localStorage.getItem('uid')!,
+					),
+				);
+				const data = await response.json();
+				setIsSingedIn(data);
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
+		fetchData();
+	}, []);
 	return (
 		<Flex className="flex-col bg-black text-white h-screen w-full">
 			<MenuBar isSignedIn={isSingedIn} />
