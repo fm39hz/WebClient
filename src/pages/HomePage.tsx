afbc745af396bd579707c2670b538c37ec173461
fm39hz <hitpoint2k3@gmail.com>
2023-12-18 14:51:44 +0700

    added key identifier

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,9 +10,9 @@ const HomePage = (props: HomePageProps) => {
 		<Flex className="flex-col items-center gap-8 overflow-auto">
 			<Spacer />
 			<List className=" flex flex-col items-center gap-4">
-				{props.products.map((item) => (
-					<List className="flex flex-row gap-4">
-						<ProductCard {...item}></ProductCard>
+				{props.products.map((item, index) => (
+					<List key={index} className="flex flex-row gap-4">
+						<ProductCard key={item.id} {...item}></ProductCard>
 					</List>
 				))}
 			</List>
