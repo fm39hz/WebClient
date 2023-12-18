a9bfe9fe0546eaa28f3baa93637f99521f456e27
fm39hz <hitpoint2k3@gmail.com>
2023-12-18 19:51:24 +0700

    added border

--- src/components/MenuBar/Items/SignInButton/SignInIcon.tsx
+++ src/components/MenuBar/Items/SignInButton/SignInIcon.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const SignInIcon = () => (
 	<Link
-		className="flex flex-row items-center rounded-xl my-0.5 bg-inherit mr-2"
+		className="flex flex-row items-center rounded-xl my-0.5 bg-inherit mr-2  border border-l-purple-100"
 		to="/SignIn"
 	>
 		<UserIcon className="w-8 h-8 mx-2" />
