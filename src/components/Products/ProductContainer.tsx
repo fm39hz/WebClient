893497e71e0f0c0cbd4a462a7856af7589d83524
fm39hz <hitpoint2k3@gmail.com>
2023-12-21 16:40:02 +0700

    wrap Product

--- src/components/Products/ProductContainer.tsx
+++ src/components/Products/ProductContainer.tsx
@@ -1,20 +1,34 @@
 import { Flex, Text } from '@chakra-ui/react';
 import ProductCard, { ProductProps } from './ProductCard';
+import { GetApi, ServiceEndPoint } from 'Constant';
+import { useState, useEffect } from 'react';
 
 type ProductContainerProps = {
-	products: ProductProps[];
 	title: string;
 };
 
 const ProductContainer = (props: ProductContainerProps) => {
+	const [products, setProducts] = useState({} as ProductProps[]);
+	const [isLoading, setIsLoading] = useState(true);
+	const fetchData = async () => {
+		try {
+			const _products = await fetch(GetApi(ServiceEndPoint.products)!);
+			setProducts(await _products.json());
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	};
+	useEffect(() => {
+		fetchData();
+	}, []);
 	return (
 		<Flex className="flex-col m-8 bg-white p-4 rounded-md w-fit">
 			<Text className="text-2xl text-black font-bold mb-1 ml-6">
 				{props.title}
 			</Text>
 			<Flex className="flex-wrap gap-4 justify-start m-4">
-				{props.products.map((product) => (
-					<ProductCard key={product.id} {...product} />
+				{Object.values(products).map((product) => (
+					<ProductCard key={product.id} id={product.id} />
 				))}
 			</Flex>
 		</Flex>
